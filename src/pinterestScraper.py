9705ecd36019283061fe4a200c5aa7ca90c42107
Lugar95 <luke.gardiner.95@gmail.com>
2021-10-07 18:18:29 +0100

    Extend code to include all categories in /ideas. Add new attributes, add get_catergory_links, add create_folders.

--- src/pinterestScraper.py
+++ src/pinterestScraper.py
@@ -6,13 +6,30 @@ import os
 
 # Let's define our class
 class PinterestScraper:
-    def __init__(self, category, root):
-        self.category = category # Animals- animals/925056443165/
-        self.root = root #pinterest.co.uk/ideas/
+    def __init__(self, root):
+        self.category = None # Animals- animals/925056443165/
+        self.root = root #pinterest.co.uk/
         self.driver = webdriver.Chrome()
         self.image_set = set()
+        self.category_link_list = []
+        self.save_path = None
         # self.image_links = [] Initialize links, so if the user calls for get_image_source, it doesn't throw an error
         
+    def get_category_links(self):
+        self.driver.get(self.root)
+        sleep(2)
+        categories = self.driver.find_elements_by_xpath('//div[@data-test-id="interestRepContainer"]//a')
+        self.category_link_list = [link.get_attribute('href') for link in categories]
+
+    def create_folders(self):
+        path = '..'
+        for category in self.category_link_list:
+            name = category.split('/')[4]
+            print(name)
+            if not os.path.exists(f'../{name}'):
+                os.makedirs(f'../{name}')
+
+
 #     def extract_links(self) -> None:
 #         self.driver.get(self.root + self.category)
 #         category_list = self.driver.find_elements_by_xpath('//div[@class="jzS un8 P9E"]')
@@ -51,15 +68,18 @@ class PinterestScraper:
         sleep(2)
         # self.driver.execute_script("document.body.style.zoom='25%'")
         # sleep(2)
-        for _ in range(5):
+        for _ in range(3):
             self.driver.execute_script(f"window.scrollTo(0, {Y})")
             sleep(1)
-            element = self.driver.find_element_by_xpath('//div[@data-test-id="grid"]//div[@class="vbI XiG"]')
-            image_list = element.find_elements_by_xpath('//div[@class="Yl- MIw Hb7"]//img')
-            # ('//div[@data-test-id="grid"]//div[2]//img')
-            print(len(image_list))
-            self.image_set.update([link.get_attribute('src') for link in image_list])
-            print(len(self.image_set), 'image set')
+            try:
+                container = self.driver.find_element_by_xpath('//div[@data-test-id="grid"]//div[@class="vbI XiG"]')
+                image_list = container.find_elements_by_xpath('//div[@class="Yl- MIw Hb7"]//img')
+                # ('//div[@data-test-id="grid"]//div[2]//img')
+                print(len(image_list))
+                self.image_set.update([link.get_attribute('src') for link in image_list])
+                print(len(self.image_set), 'image set')
+            except: 
+                print('Some error, likely no pics')
         # sleep(5)
         # self.driver.refresh()
         # sleep(5)ß
@@ -75,17 +95,22 @@ class PinterestScraper:
         # self.src = link.get_attribute('src')
         self.src = link
 
-    def download_images(self, i, save_path) -> None:
-        filename = self.category.split('/')[0]
-        urllib.request.urlretrieve(self.src, f"{save_path}/{filename}_{i}.jpg")
-    
-    def get_category_images(self, save_path: str) -> None:
-        self.extract_links()
-        for i, link in enumerate(self.image_set):
-            if '75x75' not in link:
-                self.get_image_source(link)
-                self.download_images(i ,save_path)
-        self.links = []
+    def download_images(self, i) -> None:
+        # filename = self.category.split('/')[0]
+        urllib.request.urlretrieve(self.src, f"../{self.save_path}/{self.save_path}_{i}.jpg")
+
+    def get_category_images(self) -> None:
+        self.get_category_links()
+        self.create_folders()
+        for category in self.category_link_list:
+            self.category = category.replace('https://www.pinterest.co.uk/ideas/', "")
+            self.extract_links()
+            self.save_path = f'{self.category.split("/")[0]}'
+            for i, link in enumerate(self.image_set):
+                if '75x75' not in link:
+                    self.get_image_source(link)
+                    self.download_images(i)
+            self.image_set = set()
         self.driver.quit()
 
 if __name__ == "__main__":
@@ -100,9 +125,9 @@ if __name__ == "__main__":
     #     # print(init_idx)
     #     # pinterestScrapper.get_category_images(f'D:\AiCore-perso\Projects\Pintrest-webscraping-project\data\{name}', init_idx)
     
-    pinterest_scraper = PinterestScraper('animals/925056443165/', 'https://www.pinterest.co.uk/ideas/')
-    name = 'animals'
-    if not os.path.exists(f'../{name}'):
-        os.makedirs(f'../{name}')
+    pinterest_scraper = PinterestScraper('https://www.pinterest.co.uk/ideas/')
+    # name = 'animals'
+    # if not os.path.exists(f'../{name}'):
+    #     os.makedirs(f'../{name}')
     # print(pinterest_scraper.src)
-    pinterest_scraper.get_category_images(f'../{name}')
+    pinterest_scraper.get_category_images()
