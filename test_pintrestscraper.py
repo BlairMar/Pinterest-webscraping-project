ae60cd9077ab0a6acb7e2904cdc8a6e1350736a4
Daniel Zakaiem <danielzakaiem@yahoo.co.uk>
2021-12-01 15:17:56 +0000

    Neaten up code

--- test_pintrestscraper.py
+++ test_pintrestscraper.py
@@ -1,4 +1,5 @@
 from types import MethodDescriptorType
+from urllib.request import DataHandler
 from hypothesis.core import TestFunc
 from selenium import webdriver     ###########
 from webdriver_manager.chrome import ChromeDriverManager #########
@@ -7,10 +8,7 @@ from src import pinterestScraper
 from hypothesis import strategies as st
 from hypothesis import given       
 import hypothesis.strategies as st
-import random
-import requests
 from collections import defaultdict
-import time
 import unittest
 driver = webdriver.Chrome(ChromeDriverManager().install()) 
 webdriver.Chrome('/Users/danielzakaiem/Downloads/chromedriver')
@@ -18,6 +16,7 @@ import unittest.mock
 from unittest.mock import patch
 import os
 #TO DO:
+#do other tests - like testing space complexity of each method 
 #time each Method
 #test that error is NOT thrown for certain methods
 # neaten up code 
@@ -145,8 +144,11 @@ class PinterestScraperTestCase(unittest.TestCase):
  
  #makes sure entire method is run - haveto type in RDS details manually
     def test_create_RDS (self):
-         test = self.pinterest_scraper.create_RDS()
-         self.assertEqual(test, True)
+        """
+        This function is used to test if the entire method is run properly
+        """
+        test = self.pinterest_scraper.create_RDS()
+        self.assertEqual(test, True)
 
 
 #     #not sure what inut should be????
@@ -240,20 +242,6 @@ class PinterestScraperTestCase(unittest.TestCase):
     #      test = self.pinterest_scraper._grab_images_src() 
     #      self.assertEqual (test, True)
 
-# #
-# # #not sure what input is here
-# #     def test_grab_title (self):
-# #         pass
-
-# # def test_grab_description (self):
-# #     pass
-    
-# # def test_grab_user_and_count (self):
-# #     pass
-
-# def grab_tags (self):
-#     pass 
-
 
 #    #doesnt work- try and test if image is downloaded remotely/locally
 #     def test_download_image(self):
@@ -270,3 +258,34 @@ class PinterestScraperTestCase(unittest.TestCase):
     
 if __name__ == "__main__":
     unittest.main(argv= [''],verbosity=2,exit =False)   #"run all of the unittests we have defined"
+
+
+# TO DO:
+#test RDS function when user inputs locally 
+
+
+
+scraper collects Data
+and stores on files made in computer or s3 bucket based on user input 
+
+#categories to saves images : ask if they wanna download any of the images - offers depedning on how many categories they have selected in earlier functions ovs- then dictionary is empty - thereis a dictionary called self.catimages_to_save - which is for all images we will download - the image names will have true or false next to all of them - ones with true will be downloaded 
+#savetocloud or loacal : asks if wanna download to s3 bucket- if they say bno- gets saved to local machine- if they say yes- have to type in s3 bucket credentials to it is then saved to the s3 bucket (only works with a public bucket)- then makes a list of categories taht we want to save to the bucket for s_3_list - the vlaues on this will be saved on s3 buckt, the rest which arent on list will be stored locally 
+#initialise counter : countes how which pages we have been to in a certain category (in a counter_dict) so that we wont repeat a page in the future- for now, we set all keys to 0 - the dictionary with keys-categories and values- a number wich goes up as u go over more pages in a specific category- this method gives them all values of 0 to start with- these temp files are 
+#initialise_local_folders: creating a folder called 'data' (to put all scraped images) unless its being saved to s3 bucket - then  creates temp folder within the data folder based on categories selected (eg: tmp_animals and a temp_hallowean WITHIN THE data folder)
+#check_for_logs:
+#grab_image_srcs:  same shit that extract_links does - it extracts the src links for the categories selcted - it will scroll down each categoru selected - collecting all hrefs- placing in python set 
+#extract_links : so we basically have hrefs for each category saved in a set in python
+#grab_page_data: also puts loads of functions together - basically form each href link (in our href set), we grab different data points like 'title', 'description' from each href page AND collects other info like if the - by the end of this method AND data_dump function  - essentially, u end up with all diffferent data points collected in a seperate temprary older per gategory with images downloaded (if you have slected to do so)- note: all the data collected per each href link is given in a json format
+# #create_log: creates a new files called 'log.json' - this is used incase people want to run the scraper twice- it'll basically save the hrefs that u already used to u dont revisit (the href page)if run scraper again  and a 'recent-save-log.json: which tells you that if u wanna rerun, itll continue where it left off in scraping the data points - by looking at the temp files for each category and c
+#check for logs: makes sure these files exist^
+#at end got json files for each category  saved in bucket or locally (whichever u choose)
+#then we go to create_RDS: asks if u wanna create a database locally on pgadmin, but u can also choose to do it with an aws database 
+#now next steps would be to: run this entire scraper (script) in a docker container (with cron jobs every few weeks) on an EC2 instance 
+#w ewill the set up prometheus to monitor the EC2 instance basiclly 'does something'
+# so we need to:
+# set up prometheus to track EC2 instance:
+#pascals is currently running the scraper docker image on his EC2 instance on a cron job 
+#so basically set up prometheus to track EC2 on pascals ECS instance - acc do it all urself- connect to ur ec2 insance- by connecting cloudwatch to it, then connecting cloud watch with prometheus 
+#basically find a way of makign ec2 instance -make it obuntu 20.04 version when making the instance!- run the docker file on it (with crontab - which makes it run the docker container (our scraper) every 10 minutes- connect prometheus and grafana 
+#NOTE: this docker container we have already has all the preset values 
+#securecopy image created from gthub (EC2-Ubuntu-20.04) (SO NOT A CONTAINER IN DOCKER, INSTEAD, ITS AN IMAGE AS A FILE) which is in our webscraping folderinto ec2 instance 
