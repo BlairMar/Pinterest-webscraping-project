0a9bd0991cad726d0ef7d13274c5ea60a20977e4
Daniel Zakaiem <danielzakaiem@yahoo.co.uk>
2022-01-11 21:52:56 +0000

    Commit solely for merging purposes

--- src/pinterestScraper.py
+++ src/pinterestScraper.py
@@ -8,7 +8,6 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC 
 import json
-
 from sqlalchemy.engine.base import Engine
 # from webdriver_manager.chrome import ChromeDriverManager
 import boto3 
